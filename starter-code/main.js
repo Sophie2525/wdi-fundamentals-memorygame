00d9f01fd78c86c498b2f05317d35bf83eeaab15
Sophie Downey <Sophster@Sophies-MacBook-Pro-3.local>
2017-03-15 03:02:42 +0000

    created game additions

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,9 +1,10 @@
 console.log("JS file is connected to HTML! Woo!")
 
-var CardOne = "queen";
+/*var CardOne = "queen";
 var CardTwo = "queen";
 var CardThree = "king";
 var CardFour = "king";
+*/
 
 /*if (CardTwo === CardFour){
 	alert("You found a match!")
@@ -12,14 +13,42 @@ var CardFour = "king";
 }
 */
 
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
 var gameBoard = document.getElementById('game-board');
 
-var createCards = function() {
-		for (var i=0; i<4; i++){
+var createBoard = function() {
+	for (var i=0; i<cards.length; i++) {
 		var cardNew = document.createElement('div');
 		cardNew.className = 'card';
+		cardElement.setAttribute('data-card', cards[i]);
+		cardElement.addEventListener('click', isTwoCards);
 		gameBoard.appendChild(cardNew);
+		gameBoard.appendChild(cardElement);
 	}
 }
 
-	createCards(4);
+createBoard(4);
+
+var isTwoCards = function(){
+	cardsInPlay.push(this.getAttribute('data-card'));
+	if (this.getAttribute('data-card') === 'King') {
+		this.innerHTML = '<img src="king.jpg" alt="King of the Jungle" />';
+	} else {
+		this.innerHTML = '<img src="queen.jpg" alt="Queen Bee" />'
+	}
+	if (cardsInPlay.length === 2) {
+		isMatch (cardsInPlay);
+		cardsInPlay = [];
+	}
+}
+
+var isMatch = function(){
+	if (cards[0] === cards[1] || cards[2] === cards[3]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.")
+	}
+}
+
+createBoard()
