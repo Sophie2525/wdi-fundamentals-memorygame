8c7512dc37290703f4e72db63e93b6dfdf74127f
Sophie Downey <Sophster@Sophies-MacBook-Pro-3.local>
2017-03-15 00:59:34 +0000

    created javascript cards

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,8 +5,21 @@ var CardTwo = "queen";
 var CardThree = "king";
 var CardFour = "king";
 
-if (CardTwo === CardFour){
+/*if (CardTwo === CardFour){
 	alert("You found a match!")
 } else {
 	alert("Sorry, try again.")
 }
+*/
+
+var gameBoard = document.getElementById('game-board');
+
+var createCards = function() {
+		for (var i=0; i<4; i++){
+		var cardNew = document.createElement('div');
+		cardNew.className = 'card';
+		gameBoard.appendChild(cardNew);
+	}
+}
+
+	createCards(4);
